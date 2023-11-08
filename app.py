ca567db7b06b8bd79198e3feae88ef31baa7b768
Sinjin John <sinjin.hotlinebling@gmail.com>
2023-11-08 13:12:03 +0530

    final changes

--- app.py
+++ app.py
@@ -1,70 +1,87 @@
 from __future__ import division, print_function
-# coding=utf-8
+import tempfile
+import pyrebase
 import sys
 import os
 import glob
 import re
 import numpy as np
-
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from keras.preprocessing import image
-
-
-
-
 from PIL import Image
-# import numpy as np
-from flask import Flask, request,render_template,redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
+import requests
+from io import BytesIO
 
-app=Flask(__name__,static_url_path='/static')
+config = {
+    "apiKey": "AIzaSyBV-wsWGwWorvM10tCl8MX4_gjq5uHC8Q8",
+    "authDomain": "asl-project-bca0c.firebaseapp.com",
+    "projectId": "asl-project-bca0c",
+    "storageBucket": "asl-project-bca0c.appspot.com",
+    "messagingSenderId": "272789461101",
+    "appId": "1:272789461101:web:43819fdf4968e2f9814764",
+    "databaseURL": None
+}
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+
+app = Flask(__name__, static_url_path='/static')
 
 MODEL_PATH = 'final-asl.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
 model.make_predict_function()
-print('Model loaded. Check http://127.0.0.1:5000/')     
-
+print('Model loaded. Check http://127.0.0.1:5000/')
 
 def model_predict(image, model):
-    img = Image.open(image)
-    img = img.resize((64, 64))
-
+    img = image.resize((64, 64))
     x = np.array(img)
     x = np.expand_dims(x, axis=0)
-
     preds = model.predict(x)
     return preds
 
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello_world():
     return render_template('index2.html')
 
-@app.route('/upload',methods=['GET'])
+@app.route('/upload', methods=['GET'])
 def uploadPage():
     return render_template('image_upload.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    # if request.method=='POST':
-    imageFile = request.files['imageFile']
-    # imagePath = "./images/"+imageFile.filename
-    # imageFile.save(imagePath)
-    preds = model_predict(imageFile, model)
-    top_prediction = np.argmax(preds)
-    class_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
+    try:
+        if 'imageFile' not in request.files:
+            return "No file part"
+        imageFile = request.files['imageFile']
+        if imageFile.filename == '':
+            return "No selected file"
 
-    # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-    result = class_labels[top_prediction]              # Convert to string
-    return render_template('image_upload.html', result=result)
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file_path = temp_file.name
+            imageFile.save(temp_file_path)
 
-# return None
+        storage.child("images/" + imageFile.filename).put(temp_file_path)
+        download_url = storage.child("images/" + imageFile.filename).get_url(None)
 
-    # return render_template('index.html')
+        # Download the image and open it with PIL
+        response = requests.get(download_url)
+        img = Image.open(BytesIO(response.content))
 
+        preds = model_predict(img, model)
+        top_prediction = np.argmax(preds)
+        class_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
+        result = class_labels[top_prediction]
 
-if __name__ =='__main__':
-    app.run(port=3000,debug=True)
+        os.remove(temp_file_path)
+        return render_template('image_upload.html', result=result)
+    except Exception as e:
+        print(e)
+        return str(e)
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
